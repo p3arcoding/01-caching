da3c3807e837f1061ed899e3d47663624d33a2f9
Pear Coding <pearcodingyt@gmail.com>
2021-06-21 19:00:55 +0100

    add caching to logged out

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ApplicationRecord
   validates_uniqueness_of :name
+  after_save :touch_events
+  has_many :events
+
+  private
+
+  def touch_events
+    events.update_all(updated_at: DateTime.now)
+  end
 end
